ba7e72f98cb9b8747b1c03521d3329f32a8d8fbb
Leslie McLeod <leslie.mcleod@ad.aie.edu>
2016-10-18 15:54:12 +1030

    Updated build with all current assets, scripts not setup yet. Scripts will be setup 19th/10/16

---
+++ dwagoons_Master_build001/Assets/Scripts/sams shitty scripts/oasisMechanic.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class oasisMechanic : MonoBehaviour 
+{
+	public Transform oasis;
+	public Vector3 oasisTargetOffset;
+	public float raiseTime = 2;
+	Vector3 oasisStart;
+	Vector3 oasisTarget;
+	private Vector3 oasisSecondTarget;
+	public Transform position1;
+	public GameObject Firstplayer;
+	public GameObject Secondplayer;
+	private float timer;
+	private BoxCollider col;
+
+	void Start()
+	{
+		oasisStart = oasis.position;
+		oasisTarget = oasisStart + oasisTargetOffset;
+		oasisSecondTarget = oasisStart;
+		col = GetComponent<BoxCollider> ();
+	}
+
+	void Update()
+	{
+		timer += Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject == Firstplayer)
+		{
+			StartCoroutine(RaiseOasis(raiseTime));
+		}
+		if(other.gameObject == Secondplayer)
+		{
+			StartCoroutine(RaiseOasis(raiseTime));
+		}
+	}
+	IEnumerator RaiseOasis(float time)
+	{
+		
+		float elapsedTime = 0;
+		while(elapsedTime < time)
+		{
+			oasis.position = Vector3.Lerp(oasisStart, oasisTarget, elapsedTime / time);
+			elapsedTime += Time.deltaTime;
+			yield return null;
+			//timer = 0;
+				oasis.position = oasisTarget;
+			col.enabled = false;
+		}
+
+		
+		
+		StartCoroutine (OasisBack (raiseTime));
+	}
+		
+	IEnumerator OasisBack(float time)
+	{
+		oasis.position = oasisTarget;
+		yield return new WaitForSeconds (5);
+		float elapsedTime = 0;
+		while (elapsedTime < time) 
+		{
+			oasis.position = Vector3.Lerp (oasisTarget, oasisSecondTarget, elapsedTime / time);
+			elapsedTime += Time.deltaTime;
+			yield return null;
+			timer = 0;
+		}
+		oasis.position = oasisStart;
+		yield return new WaitForSeconds (3);
+		col.enabled = true;
+	}
+		
+}
